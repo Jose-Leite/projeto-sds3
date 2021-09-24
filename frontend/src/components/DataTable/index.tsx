8ade23dbe18991d47c5e88c283e96c6fec8c8deb
Jose-Leite <jose.leite@circularbrain.io>
2021-09-24 10:08:39 -0300

    Mudança de cor do barra de navegação

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -9,7 +9,7 @@ const DataTable = () => {
                         <th>OT's</th>
                         <th>OS's</th>
                         <th>OD's</th>
-                        <th>Estoque</th>
+                        <th>Estoque em 30/08/21 (Kg)</th>
                         <th>Peso Reciclado (Kg)</th>
                         
                     </tr>
