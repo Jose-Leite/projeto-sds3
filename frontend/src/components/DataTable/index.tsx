0b4cc1b0ae95c64d9829bdc072b6ec95482d30ac
Jose-Leite <jose.leite@circularbrain.io>
2022-01-03 12:09:39 -0300

    Dezembro 21

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -9,24 +9,24 @@ const DataTable = () => {
                         <th>OT's</th>
                         <th>OS's</th>
                         <th>OD's</th>
-                        <th>Estoque em 30/11/21 (Kg)</th>
+                        <th>Estoque em 31/12/21 (Kg)</th>
                         <th>Peso Reciclado (Kg)</th>
                         
                     </tr>
                     <tr>
                         <td>Codel</td>
-                        <td>11</td>
-                        <td>91</td>
-                        <td>230</td>
-                        <td>11.216,397</td>
-                        <td>21.318,837</td>
+                        <td>20</td>
+                        <td>106</td>
+                        <td>264</td>
+                        <td>12.075,747</td>
+                        <td>24.092,411</td>
                     </tr>
                     <tr>
                         <td>Ecobras</td>
                         <td>0</td>
-                        <td>6</td>
+                        <td>7</td>
                         <td>9</td>
-                        <td>43.391,700</td>
+                        <td>57.892,300</td>
                         <td>3.990,700</td>
                     </tr>
                     <tr>
@@ -40,50 +40,50 @@ const DataTable = () => {
                     <tr>
                         <td>EcoTI - BA</td>
                         <td>24</td>
-                        <td>24</td>
-                        <td>12</td>
-                        <td>27.789,837</td>
-                        <td>1.681,224</td>
+                        <td>29</td>
+                        <td>18</td>
+                        <td>17.369,802</td>
+                        <td>2.169,308</td>
                     </tr>
                     <tr>
                         <td>EcoTI - SE</td>
                         <td>12</td>
-                        <td>102</td>
-                        <td>27</td>
-                        <td>28.452,853</td>
-                        <td>23.690,810</td>
+                        <td>119</td>
+                        <td>33</td>
+                        <td>29.908,353</td>
+                        <td>24.711,810</td>
                     </tr>
                     <tr>
                         <td>Natal</td>
-                        <td>113</td>
-                        <td>999</td>
-                        <td>148</td>
-                        <td>37.881,567</td>
-                        <td>635.278,293</td>
+                        <td>139</td>
+                        <td>1.189</td>
+                        <td>209</td>
+                        <td>88.196,510</td>
+                        <td>701.931,491</td>
                     </tr>
                     <tr>
                         <td>Reciclo</td>
-                        <td>274</td>
-                        <td>971</td>
-                        <td>584</td>
-                        <td>96.464,715</td>
-                        <td>976.252,512</td>
+                        <td>323</td>
+                        <td>1.042</td>
+                        <td>639</td>
+                        <td>73.954,027</td>
+                        <td>1.064.755,615</td>
                     </tr>
                     <tr>
                         <td>Reeecicle</td>
-                        <td>47</td>
-                        <td>169</td>
+                        <td>49</td>
+                        <td>175</td>
                         <td>1</td>
                         <td>26.887,103</td>
                         <td>184,200</td>
                     </tr>
                     <tr>
                         <td>Sete Ambiental</td>
-                        <td>98</td>
-                        <td>75</td>
-                        <td>1</td>
-                        <td>2.176,870</td>
-                        <td>136,130</td>
+                        <td>275</td>
+                        <td>202</td>
+                        <td>4</td>
+                        <td>12.866,691</td>
+                        <td>555,210</td>
                     </tr>
                     <tr>
                         <td>WEEE</td>
@@ -96,9 +96,9 @@ const DataTable = () => {
                     <tr>
                         <td>Zero Impacto</td>
                         <td>71</td>
-                        <td>195</td>
-                        <td>12</td>
-                        <td>28.422,197</td>
+                        <td>214</td>
+                        <td>20</td>
+                        <td>30.997,927</td>
                         <td>13.654,470</td>
                     </tr>
                     <tr>
@@ -111,11 +111,11 @@ const DataTable = () => {
                     </tr>
                     <tr>
                         <th>T O T A I S</th>
-                        <th>687</th>
-                        <th>2.638</th>
-                        <th>1.024</th>
-                        <th>302.807,289</th>
-                        <th>1.676.187,176</th>
+                        <th>950</th>
+                        <th>3.089</th>
+                        <th>1.197</th>
+                        <th>350.272,510</th>
+                        <th>1.836.045,215</th>
                     </tr>
                 </thead>
                 <tbody>
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -3,7 +3,7 @@ import Chart from 'react-apexcharts';
 const DonutChart = () => {
 
     const mockData = {
-        series: [21.319, 3.991, 0, 1.681, 23.691, 635.278, 976.253, 0.184, 0.136, 0, 13.654],
+        series: [24.092, 3.991, 0, 2.169, 24.712, 701.931, 1064.756, 0.184, 0.555, 0, 13.654],
         labels: ['Codel', 'Ecobras', 'Ecodescarte', 'EcoTI - BA', 'EcoTI - SE', 'Natal', 'Reciclo', 'Reeecicle', 'Sete Ambiental', 'WEEE', 'Zero Impacto']
     }
     
