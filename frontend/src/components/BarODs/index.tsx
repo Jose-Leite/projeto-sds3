1f567836cbb20fc7eb4486dd2168b9f580d46cff
Jose-Leite <jose.leite@circularbrain.io>
2023-02-04 14:03:31 -0300

    Correção 2 do Dash de Jan/23

--- frontend/src/components/BarODs/index.tsx
+++ frontend/src/components/BarODs/index.tsx
@@ -19,7 +19,7 @@ const BarODs = () => {
         labels: {
             categories: ['BDPWaste', 'Codel', 'Ecobras', 'Ecodescarte', 'EcoTI - BA', 'EcoTI - SE', 'Natal', 'Reciclo', 'Reeecicle', 'RFR Reciclagem' , 'Sete Ambiental', 'WEEE', 'Zero Impacto']
         },
-        series: [
+            series: [
             {
                 name: "OD's Abertas",
                 data: [0, 727, 23, 0, 49, 52, 483, 1538, 1, 69, 17, 71, 79]                  
