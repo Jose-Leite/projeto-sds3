22e1b81d5a53306a39c9875715ba78a22b814ccc
Jose-Leite <jose.leite@circularbrain.io>
2023-05-13 15:31:22 -0300

    Acerto 2 Abril23

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -3,7 +3,7 @@ import Chart from 'react-apexcharts';
 const DonutChart = () => {
 
     const mockData = {
-        series: [0, 100.779, 46.304, 0, 9.459, 25.501, 1533.558, 2288.199, 0.184, 8381.480, 0.610, 0, 80.763],
+        series: [0, 100.866, 46.304, 0, 9.459, 25.501, 1580.799, 2386.926, 0.184, 11618.140, 0.610, 0, 80.763]
         labels: ['BDPWaste', 'Codel', 'Ecobras', 'Ecodescarte', 'EcoTI - BA', 'EcoTI - SE', 'Natal', 'Reciclo', 'Reeecicle', 'RFR Reciclagem' , 'Sete Ambiental', 'WEEE', 'Zero Impacto']
     }
     
